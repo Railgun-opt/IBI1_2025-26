1a4364ba0b9ba08c0d1f493c700c535eb5b8c2c8
Railgun <2664048613@qq.com>
2026-03-18 15:42:57 +0800

    edit_first

--- Practical5/Gene_Expression.py
+++ Practical5/Gene_Expression.py
@@ -1,6 +1,7 @@
 import matplotlib.pyplot as plt
 Genes = {"TP53": 12.4, "EGFR": 15.1, "BRCA1": 8.2, "PTEN": 5.3, "ESR1": 10.7}
 Genes["MYC"] = 11.6
+print(Genes)
 
 plt.bar(Genes.keys(), Genes.values())
 plt.xlabel("Genes")
